56a79f816a6658325b057c6158f9fd79ec445193
Khoa Hoang <admin@khoahoang.com>
2021-11-24 19:41:44 -0800

    rpi-bt-firmware.mk: rpi-wifi-firmware.mk: bump rpi firmware binary blob version
    Signed-off-by: Khoa Hoang <admin@khoahoang.com>

--- buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
+++ buildroot/package/rpi-wifi-firmware/rpi-wifi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_WIFI_FIRMWARE_VERSION = 9c0ffe9a7f0753b36ed88f7981905a989d940ea9
+RPI_WIFI_FIRMWARE_VERSION = 3888ba29898bb3f056d5f1eb283cb8de4c533bef
 RPI_WIFI_FIRMWARE_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware-rpi,$(RPI_WIFI_FIRMWARE_VERSION))
 RPI_WIFI_FIRMWARE_LICENSE = PROPRIETARY
 RPI_WIFI_FIRMWARE_LICENSE_FILES = LICENCE.broadcom_bcm43xx
